31fb2391802b1da253604ae3014e2f1d33f67d4c
Juliet <agutu@gmail.com>
2023-02-26 03:38:44 +0300

    add js to upload file

---
+++ uploader.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form"),
+fileInput = form.querySelector(".file-input"),
+progressArea = document.querySelector(".progress-area"),
+uploaderArea = document.querySelector(".uploaded-area");
+
+form.addEventListener("click", ()=>{
+    fileInput.click();
+});
+
+fileInput.onchange = ({target})=>{
+    let file = target.files[0];
+    if(file){
+        let fileName = file.name;
+        console.log(fileName);
+        uploadFile(fileName);
+
+    }
+}
+    
